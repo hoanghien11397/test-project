09a0fa18f7f1df8b3730f1eb29c3696256899f56
hoang thi hien <hoanghien110397@gmail.com>
2019-09-03 12:30:48 +0700

    hihi

---
+++ resources/views/master-layout.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <base href="{{asset('')}}">
+    <title>Document</title>
+
+
+
+
+    <!-- libary css -->
+    
+<!--===============================================================================================-->
+<link rel="icon" type="image/png" href="../public/images/icons/favicon.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../public/fonts/themify/themify-icons.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/slick/slick.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/vendor/lightbox2/css/lightbox.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../public/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../public/css/main.css">
+<!--===============================================================================================-->
+
+
+
+    <!-- libary font -->
+
+
+
+
+    <!-- custom css -->
+
+       
+ 
+       
+</head>
+
+<body>
+
+
+    @include('header')
+    @yield('content')
+    @include('footer')
+
+    <!-- libary js -->
+ 
+
+    <!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/bootstrap/js/popper.js"></script>
+	<script type="text/javascript" src="../vendor/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/daterangepicker/moment.min.js"></script>
+	<script type="text/javascript" src="../vendor/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/slick/slick.min.js"></script>
+	<script type="text/javascript" src="../public/js/slick-custom.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/parallax100/parallax100.js"></script>
+	<script type="text/javascript">
+        $('.parallax100').parallax100();
+	</script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script type="text/javascript" src="../vendor/vendor/lightbox2/js/lightbox.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../public/js/main.js"></script>
+
+    
+   
+
+    
+    <!-- custom js -->
+
+</body>
+
+</html>
+
